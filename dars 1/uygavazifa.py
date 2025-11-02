2ac6c0d834b2a058977eaefa16b8e80d8fccc5b6
me-xnr <ravshanovichhumoyun@gmail.com>
2025-11-02 18:55:48 +0500

    yangi faylar qushildi
    1 chi dars qushildi

---
+++ dars 1/uygavazifa.py
@@ -0,0 +1,188 @@
+# 1chi masala
+
+# a = int(input("a:"))
+# result = a > 0
+# print(result)
+
+# 2chi masala
+
+# a = int(input("a:"))
+# result = a % 2 == 1
+# print(result)
+
+# 3chi masala
+
+# a = int(input("a:"))
+# result = a % 2 == 0
+# print(result)
+
+
+# 4chi masala
+
+# a = int(input("a:"))
+# b = int(input("b:"))
+# result = a > 2
+# result1 = b <= 3
+# print(f'a natijasi: {result} b natijasi: {result1}')
+
+# 5chi masala
+
+# a = int(input("a:"))
+# b = int(input("b:"))
+# c = int(input("c:"))
+# nat = a<b<c
+# print(nat)
+
+# 8 masala
+# a = int(input("a:"))
+# b = int(input("b:"))
+# nat = a % 2 == 1 and b % 2 == 1
+# print(f'natija: {nat}')
+
+# 9 chi masala
+
+# a = int(input("a:"))
+# b = int(input("b:"))
+# nat = a % 2 == 1 or b % 2 == 1
+# print(f'natija: {nat}')
+
+# 11 chi masala
+
+# a = int(input("a:"))
+# b = int(input("b:"))
+# nat = ( a % 2 == 0 and b % 2 == 0) or ( a % 2 == 1 and b % 2 == 1)
+# print(nat)
+
+# 17 chi masala
+# a = int(input("son kirit:"))
+# nat = 0 < a < 999 and a % 2 == 1
+# print(nat)
+
+# 18 chi masala
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# c = int(input("son kirit:"))
+# nat = a == b or b == c or a == c
+# print(nat)
+
+# 19 chi masala
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# c = int(input("son kirit:"))
+# nat = a != b and b != c and a != c
+# print(nat)
+
+
+# 20 chi masala
+
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# c = int(input("son kirit:"))
+# nat = a != b and a != c and b != c and c != a
+# print(nat)
+
+# 21 masala
+
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# c = int(input("son kirit:"))
+# natija = a < b and b < c
+# print(natija)
+
+
+
+# a = int(input("a:"))
+# b = int(input("b:"))
+# c = int(input("c:"))
+# plus = 0
+# if a > 0:
+#     plus += 1
+# if b > 0:
+#     plus += 1
+# if c > 0:
+#     plus += 1
+# if plus == 2:
+#     print("true")
+# else:
+#     print("false")
+
+
+# 22 chi malasa
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# c = int(input("son kirit:"))
+# natija = a < b and b < c or a > b and b > c
+# print(natija)
+
+# 23 chi masala
+
+# a = int(input("enter number:"))
+# x1 = a % 10
+# x2 = a // 100 % 10
+# nat = x1 == x2
+# print(nat)
+
+
+# 24 chi masala
+
+# x= int(input("x ga qiymat  kiriting:"))
+# y = int(input("y ga qiymat  kiriting:"))
+# nat1 = x < 0 and y > 0
+# print(nat1)
+
+# 25 chi masala
+
+# x= int(input("x ga qiymat  kiriting:"))
+# y = int(input("y ga qiymat  kiriting:"))
+# natija = x > 0 and y < 0
+# print(natija)
+
+
+# 27 chi masala
+# x = int(input("x ga qiymat  kiriting:"))
+# y = int(input("y ga qiymat  kiriting:"))
+# natija = x < 0 and y > 0 or x < 0 and y < 0
+# print(natija)
+
+
+# 28 chi masal
+
+# x= int(input("x ga qiymat  kiriting:"))
+# y = int(input("y ga qiymat  kiriting:"))
+# natija = x > 0 and y > 0 or x < 0 and y < 0
+# print(natija)
+
+
+# 30 chi masala
+
+# a = int(input("enter number:"))
+# b = int(input("enter number:"))
+# c = int(input("enter number:"))
+# result = a == b and b == c and c == a
+# print(result)
+
+# 32 masala
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+#
+# result = (a == b or b == c or a == c) and not (a == b and b == c)
+# print("Uchburchak teng yonli:", result)
+
+# 33 chi masala
+
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+# result = ( a + b > c) and ( b + c > a) and ( c + a > b)
+# print(result)
+
+
+
